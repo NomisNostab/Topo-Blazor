4d681a750dcb63b0b9e77513d56257adf1f5842b
Simon <simon@batson.id.au>
2024-11-11 21:44:48 +1100

    Issue-193: Update cached wall chart model member name for suppress last name

--- Topo/Services/WallchartService.cs
+++ Topo/Services/WallchartService.cs
@@ -29,16 +29,24 @@ namespace Topo.Services
 
         public async Task<List<WallchartItemModel>> GetWallchartItems(string selectedUnitId)
         {
+            var members = await _membersService.GetMembersAsync(selectedUnitId);
             var cachedWallchartItems = _storageService.CachedWallchartItems.Where(cm => cm.Key == selectedUnitId).FirstOrDefault().Value;
             if (cachedWallchartItems != null)
+            {
+                // Update cache member name
+                foreach (var item in cachedWallchartItems)
+                {
+                    var member = members.Where(m => m.id == item.MemberId).FirstOrDefault();
+                    item.MemberName = $"{member?.first_name} {_membersService.GetMemberLastName(selectedUnitId, item.MemberId).Result}";
+                }
                 return cachedWallchartItems;
+            }
 
             var section = _storageService.Section;
             approvals = await _approvalService.ReadApprovalListFromLocalStorage(selectedUnitId);
             await _terrainAPIService.RevokeAssumedProfiles();
 
             var wallchartItems = new List<WallchartItemModel>();
-            var members = await _membersService.GetMembersAsync(selectedUnitId);
             foreach (var member in members.Where(m => m.isAdultLeader == 0).OrderBy(m => m.last_name).ThenBy(m => m.first_name))
             {
                 var wallchartItem = new WallchartItemModel();
@@ -142,6 +150,7 @@ namespace Topo.Services
                 }
                 wallchartItem.OASStageProgressions = oasResults.results.Where(o => o.status == "awarded" && o.section == _storageService.Section).Count();
                 // Member
+                wallchartItem.MemberId = member.id;
                 wallchartItem.MemberName = $"{member.first_name} {_membersService.GetMemberLastName(selectedUnitId, member.id).Result}";
                 wallchartItem.MemberPatrol = member.patrol_name;
                 wallchartItem.IntroToScouting = await GetIntroToScouting(member.id);
@@ -202,162 +211,6 @@ namespace Topo.Services
 
                 wallchartItem.PeakAward = 0;
                 wallchartItems.Add(wallchartItem);
-            }
-            _storageService.CachedWallchartItems.Add(new KeyValuePair<string, List<WallchartItemModel>>(selectedUnitId, wallchartItems));
-            return wallchartItems;
-
-
-            var getGroupLifeResultModel = await _terrainAPIService.GetGroupLifeForUnit(selectedUnitId);
-            if (getGroupLifeResultModel.results.Length == 0)
-            {
-                return new List<WallchartItemModel>();
-            }
-            foreach (var result in getGroupLifeResultModel.results)
-            {
-                //await _terrainAPIService.AssumeProfile(result.member_id);
-                //var getMemberLogbookMetrics = await _terrainAPIService.GetMemberLogbookMetrics(result.member_id);
-                //var totalNightsCamped = getMemberLogbookMetrics.results.Where(r => r.name == "total_nights_camped").FirstOrDefault()?.value ?? 0;
-                //var totalNightsCampedInSection = getMemberLogbookMetrics.results.Where(r => r.name == "total_nights_camped" && r.last_updated >= result.intro_to_section).FirstOrDefault()?.value ?? 0;
-                //var totalKmsHiked = (getMemberLogbookMetrics.results.Where(r => r.name == "total_distance_hiked").FirstOrDefault()?.value ?? 0) / 1000.0f;
-                //var member = await _membersService.GetMember(selectedUnitId, result.member_id);
-
-                var wallchartItem = new WallchartItemModel();
-                //wallchartItem.MemberName = result.name;
-                //wallchartItem.MemberPatrol = member.patrol_name;
-                //wallchartItem.IntroToScouting = result.intro_to_scouts;
-                //wallchartItem.IntroToSection = result.intro_to_section;
-                //var currentMilestone = result.milestone.milestone;
-                //foreach (var milestone in result.milestones.OrderBy(m => m.milestone))
-                //{
-                //    if (milestone.milestone == 1)
-                //    {
-                //        if (milestone.awarded)
-                //        {
-                //            wallchartItem.Milestone1Community = 6;
-                //            wallchartItem.Milestone1Creative = 6;
-                //            wallchartItem.Milestone1Outdoors = 6;
-                //            wallchartItem.Milestone1PersonalGrowth = 6;
-                //            wallchartItem.Milestone1Assist = 2;
-                //            wallchartItem.Milestone1Lead = 1;
-                //            wallchartItem.Milestone1Awarded = milestone.status_updated;
-                //            wallchartItem.Milestone1Presented = GetPresentedDate(result.member_id, "Milestone 1");
-                //        }
-                //        else
-                //        {
-                //            wallchartItem.Milestone1Community = milestone.participates.Where(p => p.challenge_area == "community").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone1Creative = milestone.participates.Where(p => p.challenge_area == "creative").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone1Outdoors = milestone.participates.Where(p => p.challenge_area == "outdoors").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone1PersonalGrowth = milestone.participates.Where(p => p.challenge_area == "personal_growth").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone1Assist = milestone.total_assists;
-                //            wallchartItem.Milestone1Lead = milestone.total_leads;
-                //        }
-                //    }
-                //    if (milestone.milestone == 2)
-                //    {
-                //        if (milestone.awarded)
-                //        {
-                //            wallchartItem.Milestone2Community = 5;
-                //            wallchartItem.Milestone2Creative = 5;
-                //            wallchartItem.Milestone2Outdoors = 5;
-                //            wallchartItem.Milestone2PersonalGrowth = 5;
-                //            wallchartItem.Milestone2Assist = 3;
-                //            wallchartItem.Milestone2Lead = 2;
-                //            wallchartItem.Milestone2Awarded = milestone.status_updated;
-                //            wallchartItem.Milestone2Presented = GetPresentedDate(result.member_id, "Milestone 2");
-                //        }
-                //        else
-                //        {
-                //            wallchartItem.Milestone2Community = milestone.participates.Where(p => p.challenge_area == "community").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone2Creative = milestone.participates.Where(p => p.challenge_area == "creative").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone2Outdoors = milestone.participates.Where(p => p.challenge_area == "outdoors").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone2PersonalGrowth = milestone.participates.Where(p => p.challenge_area == "personal_growth").FirstOrDefault()?.total ?? 0;
-                //            wallchartItem.Milestone2Assist = milestone.total_assists;
-                //            wallchartItem.Milestone2Lead = milestone.total_leads;
-                //        }
-                //    }
-                //    if (milestone.milestone == 3)
-                //    {
-                //        wallchartItem.Milestone3Community = milestone.participates.Where(p => p.challenge_area == "community").FirstOrDefault()?.total ?? 0;
-                //        wallchartItem.Milestone3Creative = milestone.participates.Where(p => p.challenge_area == "creative").FirstOrDefault()?.total ?? 0;
-                //        wallchartItem.Milestone3Outdoors = milestone.participates.Where(p => p.challenge_area == "outdoors").FirstOrDefault()?.total ?? 0;
-                //        wallchartItem.Milestone3PersonalGrowth = milestone.participates.Where(p => p.challenge_area == "personal_growth").FirstOrDefault()?.total ?? 0;
-                //        wallchartItem.Milestone3Assist = milestone.total_assists;
-                //        wallchartItem.Milestone3Lead = milestone.total_leads;
-                //        wallchartItem.Milestone3Awarded = milestone.awarded ? milestone.status_updated : null;
-                //        wallchartItem.Milestone3Presented = GetPresentedDate(result.member_id, "Milestone 3");
-                //    }
-                //}
-                //foreach (var oas in result.oas.highest.OrderBy(h => h.stream).ThenBy(h => h.stage))
-                //{
-                //    switch (oas.stream)
-                //    {
-                //        case "bushcraft":
-                //            wallchartItem.OASBushcraftStage = oas.stage;
-                //            break;
-                //        case "bushwalking":
-                //            wallchartItem.OASBushwalkingStage = oas.stage;
-                //            break;
-                //        case "camping":
-                //            wallchartItem.OASCampingStage = oas.stage;
-                //            break;
-                //        case "alpine":
-                //            wallchartItem.OASAlpineStage = oas.stage;
-                //            break;
-                //        case "cycling":
-                //            wallchartItem.OASCyclingStage = oas.stage;
-                //            break;
-                //        case "vertical":
-                //            wallchartItem.OASVerticalStage = oas.stage;
-                //            break;
-                //        case "aquatics":
-                //            wallchartItem.OASAquaticsStage = oas.stage;
-                //            break;
-                //        case "boating":
-                //            wallchartItem.OASBoatingStage = oas.stage;
-                //            break;
-                //        case "paddling":
-                //            wallchartItem.OASPaddlingStage = oas.stage;
-                //            break;
-                //    }
-                //}
-                //wallchartItem.NightsCamped = (int)totalNightsCamped;
-                //wallchartItem.NightsCampedInSection = (int)totalNightsCampedInSection;
-                //wallchartItem.KMsHiked = (int)totalKmsHiked;
-                //wallchartItem.OASStageProgressions = result.oas.total_progressions;
-                var siaResultModel = await _terrainAPIService.GetSIAResultsForMember(result.member_id);
-                var awardedSIAProjects = siaResultModel.results.Where(r => r.section == section && r.status == "awarded");
-                var awardedSIAProjectsByArea = awardedSIAProjects.GroupBy(p => p.answers.special_interest_area_selection);
-                foreach (var area in awardedSIAProjectsByArea)
-                {
-                    switch (area.Key)
-                    {
-                        case "sia_adventure_sport":
-                            wallchartItem.SIAAdventureSport = area.Count();
-                            break;
-                        case "sia_art_literature":
-                            wallchartItem.SIAArtsLiterature = area.Count();
-                            break;
-                        case "sia_environment":
-                            wallchartItem.SIAEnvironment = area.Count();
-                            break;
-                        case "sia_stem_innovation":
-                            wallchartItem.SIAStemInnovation = area.Count();
-                            break;
-                        case "sia_growth_development":
-                            wallchartItem.SIAGrowthDevelopment = area.Count();
-                            break;
-                        case "sia_better_world":
-                            wallchartItem.SIACreatingABetterWorld = area.Count();
-                            break;
-                    }
-                }
-                wallchartItem.LeadershipCourse = result.personal_development;
-                wallchartItem.AdventurousJourney = result.adventurous_journey;
-                wallchartItem.PersonalReflection = result.personal_reflection;
-                wallchartItem.PeakAward = Math.Round(result.peak_award.total / 100d, 2);
-                wallchartItems.Add(wallchartItem);
-
-                await _terrainAPIService.RevokeAssumedProfiles();
             }
             _storageService.CachedWallchartItems.Add(new KeyValuePair<string, List<WallchartItemModel>>(selectedUnitId, wallchartItems));
             return wallchartItems;
